730111f2175c8f04c48534840e6e2a0279036c9f
Quintaneishon <ajitzi.quintana@gmail.com>
2019-11-30 23:41:16 -0600

    Updates

---
+++ precache-manifest.f187c6fcf2d06145c74036b631e50a90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39659294a57058f60ed91c4d6a2a9555",
+    "url": "/signals/index.html"
+  },
+  {
+    "revision": "f407a9d43c2003ae1ab2",
+    "url": "/signals/static/css/main.ba64255c.chunk.css"
+  },
+  {
+    "revision": "63c39bafc025a9bf096b",
+    "url": "/signals/static/js/2.9b2998f5.chunk.js"
+  },
+  {
+    "revision": "f407a9d43c2003ae1ab2",
+    "url": "/signals/static/js/main.99daf662.chunk.js"
+  },
+  {
+    "revision": "bb2b4d17c0d6e2cf5447",
+    "url": "/signals/static/js/runtime-main.8b5c6f48.js"
+  }
+]);
